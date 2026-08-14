f151f6ed327f9ef64a76d16ba3145368559526a2
ilias iosifidis <iosifidis90@gmail.com>
2026-08-14 16:54:32 +0300

    ITEM 39: PREFER ANNOTATIONS TO NAMING PATTERNS (mid-section)

---
+++ src/chapter6_EnumsAndAnnotations/overrideAnnotation/Bigram.java
@@ -0,0 +1,31 @@
+package chapter6_EnumsAndAnnotations.overrideAnnotation;
+
+import java.util.HashSet;
+import java.util.Set;
+
+// Buggy
+public class Bigram {
+  private final char first;
+  private final char second;
+
+  public Bigram(char first, char second) {
+    this.first = first;
+    this.second = second;
+  }
+  public boolean equals(Bigram b){
+    return b.first == first && b.second == second;
+  }
+  public int hasCode(){
+    return 31 * first + second;
+  }
+
+  static void main() {
+    Set<Bigram> s = new HashSet<>();
+    for (int i = 0; i < 10; i++) {
+      for (char ch = 'a'; ch <= 'z'; ch++){
+        s.add(new Bigram(ch, ch));
+      }
+    }
+    System.out.println(s.size());
+  }
+}
