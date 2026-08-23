32cf7268d2a1e9a35878f14c9f30754b96ca1164
ilias iosifidis <iosifidis90@gmail.com>
2026-08-23 17:30:05 +0300

    ITEM 53: USE VARARGS JUDICIOUSLY

---
+++ src/chapter8_Methods/overloading/Simple.java
@@ -0,0 +1,37 @@
+package chapter8_Methods.overloading;
+
+public class Simple {
+  // Simple use of Varargs
+  static int sum (int... args){
+    int sum = 0;
+    for (int arg : args){
+      sum += arg;
+    }
+    return sum;
+  }
+
+  // The wrong way to use varargs to pass one or more arguments
+  static int min(int...args){
+    if (args.length == 0){
+      throw new IllegalArgumentException("too few args");
+    }
+    int min = args[0];
+    for (int i=1; i < args.length; i++){
+      if (args[i] < min){
+        min = args[i];
+      }
+    }
+    return min;
+  }
+
+  // The right wat to use varargs to pass one or more arguments
+  static int min(int firstArg, int ... remainingArgs){
+    int min = firstArg;
+    for (int arg : remainingArgs){
+      if (arg < min){
+        min = arg;
+      }
+      return min;
+    }
+  }
+}
